56d9ada8d20fc0d27654131c14b633ed7fb25a01
yelmil <dp0808@naver.com>
2025-08-15 19:11:26 +0900

    디데이 타이머 구현, 계좌번호 작성 등록

--- dev/ts/Common.ts
+++ dev/ts/Common.ts
@@ -1,4 +1,22 @@
-function test() {
-	console.log("hello");
-	console.log("hello");
+
+$(function(){
+	WeddingTimer();
+	var timer = setInterval(WeddingTimer,1000);
+});
+
+function WeddingTimer (){
+	var targetDate = new Date(2025,11,7,11,0,0);
+	var nowDate = new Date();
+	var diff = Number(targetDate) - Number(nowDate);
+
+	var day = Math.floor(diff / (1000*60*60*24));
+	var hour = Math.floor((diff / (1000*60*60))%24);
+	var min = Math.floor((diff / (1000*60))%60);
+	var sec = Math.floor(diff / 1000%60);
+	$("#Day").html(String(day));
+	$("#Day_2").html(String(day));
+	$("#Hour").html(String(hour).padStart(2,"0"));
+	$("#Min").html(String(min).padStart(2,"0"));
+	$("#Sec").html(String(sec).padStart(2,"0"));
 }
+
