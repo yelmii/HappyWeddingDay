d485e37945a73b9d98f75f4d73722e2904bf5042
yelmil <dp0808@naver.com>
2025-08-17 23:34:17 +0900

    스크립트 작성 및 계좌,전화번호 영역 업데이트

--- begin/js/Common.js
+++ begin/js/Common.js
@@ -1,6 +1,6 @@
 $(function () {
     WeddingTimer();
-    var timer = setInterval(WeddingTimer, 1000);
+    setInterval(WeddingTimer, 1000);
 });
 function WeddingTimer() {
     var targetDate = new Date(2025, 11, 7, 11, 0, 0);
@@ -9,10 +9,42 @@ function WeddingTimer() {
     var day = Math.floor(diff / (1000 * 60 * 60 * 24));
     var hour = Math.floor((diff / (1000 * 60 * 60)) % 24);
     var min = Math.floor((diff / (1000 * 60)) % 60);
-    var sec = Math.floor(diff / 1000 % 60);
+    var sec = Math.floor((diff / 1000) % 60);
     $("#Day").html(String(day));
     $("#Day_2").html(String(day));
     $("#Hour").html(String(hour).padStart(2, "0"));
     $("#Min").html(String(min).padStart(2, "0"));
     $("#Sec").html(String(sec).padStart(2, "0"));
+}
+function copyText(index) {
+    var copyText = $("#Bank_" + index).val();
+    window.navigator.clipboard.writeText(String(copyText)).then(function () {
+        $("[id^='CopyDone_']").addClass("hidden");
+        $("[id^='Copy_']").removeClass("hidden");
+        $("#CopyDone_" + index).removeClass("hidden");
+        $("#Copy_" + index).addClass("hidden");
+        setTimeout(function () {
+            $("#CopyDone_" + index).addClass("hidden");
+            $("#Copy_" + index).removeClass("hidden");
+        }, 5000);
+    });
+}
+function SlideShow(index) {
+    $("#AccountList_" + index)
+        .find("ul")
+        .slideToggle();
+    $("#BtnAccountMore_" + index).toggleClass("on");
+}
+function FadeShow() {
+    var ids = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        ids[_i] = arguments[_i];
+    }
+    var str = "";
+    for (var _a = 0, ids_1 = ids; _a < ids_1.length; _a++) {
+        var id = ids_1[_a];
+        $("#" + id).fadeToggle();
+        str == "" ? str += id : str += "," + id;
+    }
+    $("body").toggleClass("scroll-none");
 }
